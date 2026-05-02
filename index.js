6584ea18e29490c8e866598a0056c76cc5214e49
Jurek.wwwww <szymonjeziorek77@gmail.com>
2026-05-03 01:55:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`✅ Bot zalogowany jako ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
